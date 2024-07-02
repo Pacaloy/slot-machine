f67c898ccce8c2540ebbaa66404d710934d9899e
Carlo <cruz.carlo.diaz@gmail.com>
2024-07-03 00:07:27 +0800

    working slot

--- js/index.js
+++ js/index.js
@@ -1,44 +1,64 @@
-import { greet } from './script.js';
+import { IMAGE_COUNT, DEFAULT_Y_POSITION } from './utils.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   const displays = document.getElementsByClassName('display');
   const spinButton = document.getElementById('spin-button');
 
-  greet();
   initialize(displays);
   spinButton.onclick = () => spin(displays);
 });
 
 function initialize(displays) {
   for (let i = 0; i < displays.length; i++) {
-    const startDisplay = document.createElement('img');
-    startDisplay.src = './assets/img0.webp';
-    startDisplay.style.position = 'absolute';
-    startDisplay.style.top = '50%';
-    startDisplay.style.left = '50%';
-    startDisplay.style.transform = 'translate(-50%, -50%)';
-    displays[i].appendChild(startDisplay);
+    const starterImage = document.createElement('img');
+    starterImage.src = './assets/img0.webp';
+    starterImage.style.position = 'absolute';
+    starterImage.style.top = DEFAULT_Y_POSITION + '%';
+    starterImage.style.left = '50%';
+    starterImage.style.transform = 'translate(-50%, -50%)';
+    displays[i].appendChild(starterImage);
   }
 }
 
 function spin(displays) {
-  console.log('spinnnn');
+  const results = [];
 
-  console.log(displays[0]);
-  
   for (let i = 0, delay = 0; i < displays.length; i++, delay += 200) {
-    const image = displays[i].getElementsByTagName('img');
-    let position = 50;
+    results.push(Math.floor(Math.random() * IMAGE_COUNT));
+    const currentImage = displays[i].getElementsByTagName('img');
+    const interval = 5;
+    let spinTimeout = interval;
+    let yPosition = DEFAULT_Y_POSITION;
+    let imageIndex = 0;
 
     setTimeout(() => {
-      setInterval(() => {
-        position += 5;
-        image[0].style.top = position + '%';
-  
-        if (position > 130) {
-          position = -30;
+      const intervalId = setInterval(() => {
+        yPosition += 10;
+        currentImage[0].style.top = yPosition + '%';
+        spinTimeout += interval;
+
+        if (yPosition > 130) {
+          yPosition = -30;
+          imageIndex++;
+
+          if (imageIndex > IMAGE_COUNT - 1) imageIndex = 0;
+
+          displays[i].removeChild(currentImage[0]);
+          const nextImage = document.createElement('img');
+          nextImage.src = `./assets/img${imageIndex}.webp`;
+          nextImage.style.position = 'absolute';
+          nextImage.style.top = yPosition + '%';
+          nextImage.style.left = '50%';
+          nextImage.style.transform = 'translate(-50%, -50%)';
+          displays[i].appendChild(nextImage);
         }
-      }, 5)
+
+        if (spinTimeout > (1000 * (i + 1)) && results[i] === imageIndex && yPosition >= DEFAULT_Y_POSITION) {
+          currentImage[0].style.top = DEFAULT_Y_POSITION + '%';
+          clearInterval(intervalId);
+        }
+      }, interval)
     }, delay)
   }
+  console.log(results)
 }
---
+++ js/utils.js
@@ -0,0 +1,11 @@
+const IMAGE_COUNT = 6;
+const DEFAULT_Y_POSITION = 50;
+
+// const renderImage = (parent, imageIndex, yPosition) => {
+
+// }
+
+export {
+  IMAGE_COUNT,
+  DEFAULT_Y_POSITION,
+}
