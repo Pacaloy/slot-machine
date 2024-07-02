5608efb29912b3877b7a54acb4d7f34b59f66e00
Carlo <cruz.carlo.diaz@gmail.com>
2024-07-03 00:21:10 +0800

    refactor and add comments

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-import { IMAGE_COUNT, DEFAULT_Y_POSITION } from './utils.js';
+import { IMAGE_COUNT, DEFAULT_Y_POSITION, renderImage } from './utils.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   const displays = document.getElementsByClassName('display');
@@ -10,13 +10,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 function initialize(displays) {
   for (let i = 0; i < displays.length; i++) {
-    const starterImage = document.createElement('img');
-    starterImage.src = './assets/img0.webp';
-    starterImage.style.position = 'absolute';
-    starterImage.style.top = DEFAULT_Y_POSITION + '%';
-    starterImage.style.left = '50%';
-    starterImage.style.transform = 'translate(-50%, -50%)';
-    displays[i].appendChild(starterImage);
+    renderImage(displays[i])
   }
 }
 
@@ -44,13 +38,7 @@ function spin(displays) {
           if (imageIndex > IMAGE_COUNT - 1) imageIndex = 0;
 
           displays[i].removeChild(currentImage[0]);
-          const nextImage = document.createElement('img');
-          nextImage.src = `./assets/img${imageIndex}.webp`;
-          nextImage.style.position = 'absolute';
-          nextImage.style.top = yPosition + '%';
-          nextImage.style.left = '50%';
-          nextImage.style.transform = 'translate(-50%, -50%)';
-          displays[i].appendChild(nextImage);
+          renderImage(displays[i], imageIndex, yPosition);
         }
 
         if (spinTimeout > (1000 * (i + 1)) && results[i] === imageIndex && yPosition >= DEFAULT_Y_POSITION) {
--- js/utils.js
+++ js/utils.js
@@ -1,11 +1,24 @@
 const IMAGE_COUNT = 6;
-const DEFAULT_Y_POSITION = 50;
+const DEFAULT_Y_POSITION = 50; // Default Y Position of slot images in percentage.
 
-// const renderImage = (parent, imageIndex, yPosition) => {
-
-// }
+/**
+ * Renders the slot machine's current image.
+ * @param {HTMLElement} parent The element where the image is appended.
+ * @param {number} imageIndex The image's file path (ie. `./assets/img${imageIndex}.webp`).
+ * @param {number} yPosition The image's top value in percentage.
+ */
+const renderImage = (parent, imageIndex = 0, yPosition = DEFAULT_Y_POSITION) => {
+  const image = document.createElement('img');
+  image.src = `./assets/img${imageIndex}.webp`;
+  image.style.position = 'absolute';
+  image.style.top = yPosition + '%';
+  image.style.left = '50%';
+  image.style.transform = 'translate(-50%, -50%)';
+  parent.appendChild(image);
+}
 
 export {
   IMAGE_COUNT,
   DEFAULT_Y_POSITION,
+  renderImage,
 }
